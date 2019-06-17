ba075b38f942b86f424e79048730c0f900c78073
Josua Frank <frank.josua@gmail.com>
2019-06-17 20:50:59 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Externe Kochrezept-DSL in Scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.0"
---
+++ src/main/scala/main/Main.scala
@@ -0,0 +1,9 @@
+package main
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+
+}
